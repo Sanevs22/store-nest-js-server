28f97046958dc26425f2fe5a8c5114636c974503
Sanevs22 <105701264+Sanevs22@users.noreply.github.com>
2023-04-27 19:48:33 +0300

    add no db server

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Product } from './dto/product.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAll(): Product[] {
+    return this.appService.getAll();
   }
 }
